4059eec9bd0dcd93ec89f6ff713b052622fc8b9b
Christopher Hittner <chrishittner@gmail.com>
2017-10-08 00:01:35 -0400

    Conversion to Scikit-learn implementations
    I found that the scikit-learn implementation of k-means and
    dbscan are both significantly faster than my own implementation
    in python.

--- dbscan.py
+++ dbscan.py
@@ -1,87 +1,13 @@
 import numpy as np
 import sys, time
 
+from sklearn.cluster import DBSCAN
+
 
 def neighbors(points, p, eps):
     # Find the set of neighbors
     return [j for j in range(len(points))
         if np.linalg.norm(p - points[j]) <= eps]
-
-
-def dbscan(points, eps, minPts):
-    """Performs the DBSCAN clustering algorithm on a set of points.
-    """
-    C = 0
-    
-    # Mark points as unlabeled.
-    labels = [None for p in points]
-
-    Nss = [None for p in points]
-
-    for i in range(len(points)):
-
-        # If a point has already been classified, don't classify it.
-        if labels[i] != None:
-            continue
-
-        p = points[i]
-
-        # Find the set of neighbors
-        if Nss[i] is None:
-            Nss[i] = neighbors(points, p, eps)
-        Ns = Nss[i]
-
-        if len(Ns) < minPts:
-            # There are insufficient points to make a cluster.
-            # So, mark it as noise in case a cluster is found later.
-            continue
-
-        # Create the cluster
-        S = Ns
-        #labels[i] = C
-
-        inS = [x in S for x in range(len(points))]
-
-        j = 0
-        while j < len(S):
-            s = S[j]
-            q = points[s]
-
-            if labels[s] != None:
-                # The point is not noise, so ignore it.
-                j += 1
-                continue
-
-            labels[s] = C
-
-            if Nss[s] == None:
-                Nss[s] = neighbors(points, points[s], eps)
-
-            if len(Nss[s]) >= minPts:
-                # Add all of the layers.
-                S = S + [x for x in Nss[s] if x not in S]
-            
-            j += 1
-
-        
-        # Indicate that another cluster was found.
-        C += 1
-
-    # Find the representative data of the cluster.
-    categories = [[] for _ in range(C)]
-
-    # Grab the categories
-    for i in range(len(points)):
-        # Add the point
-        if labels[i] != None:
-            categories[labels[i]].append(points[i])
-
-    # Grab the means and actual categories
-
-    means = [(c, np.mean(np.array(c), axis=0))
-            for c in categories if len(c) > 0]
-    
-    return means
 
 
 def fix_photo(frames, eps=4, minPts = 3):
@@ -97,25 +23,34 @@ def fix_photo(frames, eps=4, minPts = 3):
 
     for r in range(0, shape[1]):
         row = []
-
-        for c in range(0, shape[2]):
-            # Get each of the pixels for pixel (r,c)
-            pixels = np.array([frame[r][c] for frame in frames])
-
-            # Compute the means of the photo
-            res = dbscan(pixels, eps, minPts)
-            
-            if len(res) > 0:
-                data = [len(cs) for p, cs in res]
-
-                # Find the mean with the largest number of hosted points
-                p = res[data.index(max(data))][1]
-                
-                # Add the mean to the row
-                row.append(p)
-            else:
-                row.append(np.mean(pixels, axis=0))
         
+        for c in range(0, shape[2]):
+            pixels = np.array([frame[r][c] for frame in frames])
+            
+            # Compute the means
+            db = DBSCAN(eps=eps, min_samples=minPts).fit(pixels)
+            
+            # Grab label information.
+            labels = db.labels_
+            label_set = set(labels)
+            label_count = {l : 0 for l in label_set}
+            
+            # Compute label frequency
+            for l in labels:
+                label_count[l] += 1
+            
+            # Choose a cluster.
+            choice = None
+            for l in label_set:
+                if choice is None or label_count[choice] < label_count[l] and l != -1:
+                    choice = l
+            
+            # Apply the pixel if it exists.
+            if choice is None:
+                row.append(np.mean(pixels, axis=0))
+            else:
+                row.append(np.mean(np.array([pixels[i] for i in range(len(labels)) if labels[i] == choice]), axis=0))
+                   
         # Provide the image
         image.append(row)
         
--- kmeans.py
+++ kmeans.py
@@ -1,85 +1,7 @@
 import numpy as np
 import sys, time
 
-def k_means(k, points):
-    """Performs the K-means clustering algorithm to a set of data points.
-    k - The number of clusters to generate.
-    points - The set of points to classify.
-        precondition: All points have same dimensionality.
-    return - A collection of points and their means.
-    """
-
-    min_bound = np.max(points, axis=0)
-    max_bound = np.min(points, axis=0)
-    
-    """
-    print()
-    for p in points:
-        print("point:", p)
-
-    print("min_bound:", min_bound)
-    print("max_bound:", max_bound)
-    """
-    
-    bound = list(zip(
-        min_bound,
-        max_bound
-    ))
-    
-    """
-    upper_bound = np.array([max([v[i] for v in points]) for i in range(len(points[0]))])
-    lower_bound = np.array([min([v[i] for v in points]) for i in range(len(points[0]))])
-    """
-    
-    # Initial condition: randomized means to categorize.
-    means = [
-        np.array([
-            np.random.uniform(bound[i][0], bound[i][1])
-            for i in range(len(bound))
-        ])
-        for _ in range(k)
-    ]
-
-    # We will provide categorizations of the points in the return data.
-    categories = None 
-
-    iterations = 0
-    complete = False
-
-    while not complete:
-        categories = [[] for _ in range(k)]
-
-        for p in points:
-            # We seek out the index of the nearest mean to the point.
-            m, idx = min([(np.linalg.norm(p-m), idx) for (idx, m) in enumerate(means)])
-
-            # Then, we categorize it under that mean.
-            categories[idx].append(p)
-
-        # Using these categories, we can generate a set of new means.
-        new_means = [
-                np.mean(np.array(category), axis=0) if len(category) > 0 
-                else np.array([np.random.uniform(bs[0], bs[1]) for bs in bound])
-                for category in categories
-        ]
-        
-        complete = True
-
-        for m, n in zip(means, new_means):
-            if np.linalg.norm(m-n) != 0:
-                # The means do not match, so the means have not yet converged.
-                # Store the new means, then start a new cycle.
-                means = new_means
-                complete = False
-                break
-
-        iterations += 1
-
-        # The upper bound is 2^(kd+1), where d is the dimensionality of the data
-        if complete or iterations >= len(points)**(k*len(points[0])+1):
-            break
-
-    return list(zip(means, categories))
+from sklearn.cluster import KMeans
 
 
 def fix_photo(frames, means=4):
@@ -99,18 +21,27 @@ def fix_photo(frames, means=4):
         for c in range(shape[2]):
             # Get each of the pixels for pixel (r,c)
             pixels = np.array([frame[r][c] for frame in frames])
+            
+            # Compute the means.
+            clusters = KMeans(n_clusters=means).fit(pixels)
+            
+            # Grab label information.
+            labels = clusters.labels_
+            label_set = set(labels)
+            label_count = {l : 0 for l in label_set}
+            
+            # Compute label frequency
+            for l in labels:
+                label_count[l] += 1
+            
+            # Choose a cluster.
+            choice = None
+            for l in label_set:
+                if choice is None or label_count[choice] < label_count[l]:
+                    choice = l
 
-            # Compute the means of the photo
-            res = k_means(means, pixels)
-            
-            # Find the mean with the largest number of hosted points
-            m, cs = res[0]
-            for i in range(1, len(res)):
-                if len(cs) < len(res[i][1]):
-                    m, cs = res[i]
-            
-            # Add the mean to the row
-            row.append(m)
+            # Add the value
+            row.append(clusters.cluster_centers_[l]) 
         
         # Bar progress
         bar_prog = int((bar_width * (r+1.)) / shape[1])
